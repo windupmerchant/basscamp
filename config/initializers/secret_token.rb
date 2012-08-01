cfbfb0453278864c13f3863179db4fc6cdc14163
DBC Apprentice <apprentice@devbootcamp.com>
2012-08-01 10:31:46 -0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basscamp::Application.config.secret_token = 'c735e41d59e8304400ad8ecd9afbafaf9e63bc9684fda25768f3fae28624355b895966bbc01f2d7a0d42e4a245b6e37f0109c6ef746435f19941be4ee323c5dd'
